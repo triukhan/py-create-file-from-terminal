3db706212daadfbf48cd2c436fa90f73dc48e806
Triukhan <tryukhan.d@gmail.com>
2024-05-13 23:11:29 +0300

    linter fix

--- app/create_file.py
+++ app/create_file.py
@@ -8,7 +8,7 @@ def create_directories(directory_names: list) -> None:
     os.makedirs(directory_path, exist_ok=True)
 
 
-def create_file(file_name):
+def create_file(file_name: str) -> None:
     with open(file_name, "a") as f:
         f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
         while True:
@@ -18,22 +18,23 @@ def create_file(file_name):
             f.write(text)
 
 
-def main():
-    if '-f' in sys.argv:
-        key_index_f = sys.argv.index('-f') + 1
+def main() -> None:
+    if "-f" in sys.argv:
+        key_index_f = sys.argv.index("-f") + 1
         file_path = sys.argv[key_index_f]
 
-        if '-d' in sys.argv:
-            key_index_d = sys.argv.index('-d') + 1
+        if "-d" in sys.argv:
+            key_index_d = sys.argv.index("-d") + 1
             create_directories(sys.argv[key_index_d:key_index_f - 1])
-            current_way = sys.argv[key_index_d:key_index_f - 1] + sys.argv[key_index_f:]
+            current_way = (sys.argv[key_index_d:key_index_f - 1]
+                           + sys.argv[key_index_f:])
             create_file(os.path.join(*current_way))
 
         else:
             create_file(file_path)
 
-    elif '-d' in sys.argv:
-        key_index = sys.argv.index('-d') + 1
+    elif "-d" in sys.argv:
+        key_index = sys.argv.index("-d") + 1
         create_directories(sys.argv[key_index:])
 
 
